b2a5101c4d214295c3464671b8c2b0fdd3527e1b
alnoman-robin <robinalnoman@gmail.com>
2025-10-15 22:13:43 +0600

    User Profile Card

--- phase-02/Day-15/script.js
+++ phase-02/Day-15/script.js
@@ -1,29 +1,38 @@
-let fruits = ["Apple", "Banana", "Mango"];
-
-function addFruit() {
-  let fruit = document.getElementById("fruitInput").value;
-  if (fruit) {
-    fruits.push(fruit);
-    document.getElementById("output").innerText = fruit + " added!";
-    document.getElementById("fruitInput").value = "";
-  } else {
-    document.getElementById("output").innerText = "Please enter a fruit name!";
+let users = [
+  {
+    name: "AR",
+    age: 21,
+    city: "Dhaka",
+    skills: ["HTML", "CSS", "JavaScript"],
+    image: "https://i.pravatar.cc/150?img=3",
+    profession: "Frontend Developer"
+  },
+  {
+    name: "Robin",
+    age: 22,
+    city: "Chittagong",
+    skills: ["C", "C++", "Python"],
+    image: "https://i.pravatar.cc/150?img=5",
+    profession: "Software Engineer"
+  },
+  {
+    name: "Shawon",
+    age: 20,
+    city: "Rajshahi",
+    skills: ["JavaScript", "React", "Node.js"],
+    image: "https://i.pravatar.cc/150?img=8",
+    profession: "MERN Stack Developer"
   }
-}
+];
 
-function removeFruit() {
-  if (fruits.length > 0) {
-    let removed = fruits.pop();
-    document.getElementById("output").innerText = removed + " removed!";
-  } else {
-    document.getElementById("output").innerText = "No fruits to remove!";
-  }
-}
+function generateProfile() {
+  let random = Math.floor(Math.random() * users.length);
+  let user = users[random];
 
-function showFruits() {
-  if (fruits.length > 0) {
-    document.getElementById("output").innerText = "Fruits: " + fruits.join(", ");
-  } else {
-    document.getElementById("output").innerText = "List is empty!";
-  }
+  document.getElementById("userImg").src = user.image;
+  document.getElementById("userName").innerText = `${user.name} (${user.profession})`;
+  document.getElementById("userInfo").innerText = `Age: ${user.age} | City: ${user.city}`;
+  document.getElementById("userSkills").innerText = `Skills: ${user.skills.join(", ")}`;
+
+  console.log("User Data:", user);
 }
